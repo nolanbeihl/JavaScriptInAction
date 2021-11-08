86202879289bbed28e3748b598ef7b1db20f5d1f
unknown <nolan.beihl@gmail.com>
2021-11-08 10:58:31 -0700

    working on time of day variable in javascript

--- worksheet.js
+++ worksheet.js
@@ -7,3 +7,9 @@ animalInput = prompt("What is your favorite animal?");
 colorInput = prompt("What is your favorite color?");
 
 console.log("I've never seen a " + colorInput + " " + animalInput);
+
+//# Breakfast Buritto, Drive Thru Value Menu, Steak
+
+timeOfDay = "1300"
+aFavoriteMeal = ["burrito", "Drive Thru Value Menu", "Steak"];
+
