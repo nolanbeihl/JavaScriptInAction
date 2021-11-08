5860514229db1a78698dc5710d18d9758d3daed8
unknown <nolan.beihl@gmail.com>
2021-11-08 10:41:32 -0700

    Starting of learning JavaScript

---
+++ worksheet.js
@@ -0,0 +1,9 @@
+dayOfWeek = 'Monday'
+console.log(dayOfWeek);
+dayOfWeek = 'Friday'
+console.log("I can't wait for " + dayOfWeek);
+
+animalInput = prompt("What is your favorite animal?");
+colorInput = prompt("What is your favorite color?");
+
+console.log("I've never seen a " + colorInput + " " + animalInput);
